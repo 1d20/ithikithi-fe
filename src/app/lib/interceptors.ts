8ac07a9c3ddcb5c599c5efeb39e42604670569f9
Oleksandr Nycheporuk <nycheporuk@gmail.com>
2017-12-04 00:45:23 +0200

    pdd

--- src/app/lib/interceptors.ts
+++ src/app/lib/interceptors.ts
@@ -2,8 +2,8 @@ import { Injectable } from '@angular/core';
 import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
 
-const API_URI = 'http://192.168.0.103:8000/api/';
-// const API_URI = 'http://18.216.38.56:8000/api/';
+// const API_URI = 'http://192.168.0.103:8000/api/';
+const API_URI = 'http://18.216.38.56:8000/api/';
 
 @Injectable()
 export class UrlInterceptor implements HttpInterceptor {
